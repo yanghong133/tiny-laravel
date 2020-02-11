0fd83c87af158a6fef0a7d579dad934b240a39b1
倒车的螃蟹 <yh15229262120@qq.com>
2020-02-12 00:18:22 +0800

    Create app.php

---
+++ configs/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'name' = env('APP_NAME', 'Tiny Laravel');
+	'env' = env('APP_ENV', 'Tiny Laravel');
+	'key' = env('APP_KEY', 'Tiny Laravel');
+	'debug' = env('APP_DEBUG', 'Tiny Laravel');
+	'url' = env('APP_URL', 'Tiny Laravel');
+	'timezone' => env('APP_TIME_ZOME', 'UTC'),
+];
