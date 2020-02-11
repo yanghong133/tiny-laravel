c91765c0cd5a3f7cba06cf1170c05ebbbc0e5496
倒车的螃蟹 <yh15229262120@qq.com>
2020-02-11 17:46:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
 require 'vendor/autoload.php';
-use Buersoft\Tinylaravel\Test;
-$t = new Test();
 
-echo $t->index();
+use Buersoft\Tinylaravel\Foundation\Application;
+use Buersoft\Tinylaravel\Test;
+
+$app = new Application();
+
+$app->bind('test', Test::class);
+echo $app->make('test')->index();
+var_dump($app->bindings);
+var_dump($app->instances);
+var_dump($app->getInstance());
