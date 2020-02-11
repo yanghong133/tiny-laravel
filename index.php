2c8ddb2f077c77de401967ffad6e4b41daaa40fe
倒车的螃蟹 <yh15229262120@qq.com>
2020-02-12 00:18:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 
 require 'vendor/autoload.php';
 
-use Buersoft\Tinylaravel\Foundation\Application;
-use Buersoft\Tinylaravel\Test;
+// use Buersoft\Tinylaravel\Foundation\Application;
+// use Buersoft\Tinylaravel\Test;
+// use Buersoft\Tinylaravel\Support\Facades\DB;
+// use Buersoft\Tinylaravel\Contracts\Http\Kernel as KernelContract;
+// use Buersoft\Tinylaravel\Foundation\Http\Kernel;
 
-$app = new Application();
+// $app = new Application();
+// $app->bind(KernelContract::class, Kernel::class);
+// $app->make(KernelContract::class, [$app]);
+$app = require_once __DIR__.'/bootstrap/app.php';
 
-$app->bind('test', Test::class);
-echo $app->make('test')->index();
-var_dump($app->bindings);
-var_dump($app->instances);
-var_dump($app->getInstance());
+$kernel = $app->make(Buersoft\Tinylaravel\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Buersoft\Tinylaravel\Request\Request::capture()
+);
+
+$response->send();
