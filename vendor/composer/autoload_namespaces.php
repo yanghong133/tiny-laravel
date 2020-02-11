9695a344e8f5588ebd794c8877ea18a663af9512
倒车的螃蟹 <yh15229262120@qq.com>
2020-02-11 17:47:14 +0800

    Update autoload_namespaces.php

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,9 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PhpOption\\' => array($vendorDir . '/phpoption/phpoption/src'),
-    'Parsedown' => array($vendorDir . '/erusev/parsedown'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
-    'Highlight\\' => array($vendorDir . '/scrivo/highlight.php'),
-    'HighlightUtilities\\' => array($vendorDir . '/scrivo/highlight.php'),
 );
